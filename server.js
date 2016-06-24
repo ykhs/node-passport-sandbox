f8b6708e9c2038062f3524764f59f4d59be36cbd
ykhs <yukihisa.yamada@gmail.com>
2016-06-24 21:06:49 +0900

    sign in with google

--- server.js
+++ server.js
@@ -1,15 +1,45 @@
 'use strict';
+require('dotenv').config();
 const express = require('express');
 const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 const port = process.env.PORT || 3000;
 const app = express();
 
+const googleStrategy = new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENTID,
+  clientSecret: process.env.GOOGLE_SECRET,
+  callbackURL: process.env.GOOGLE_CALLBACKURL
+}, (accessToken, refreshToken, profile, done) => {
+  console.log(`
+**accessToken**
+${accessToken}
+
+**refreshToken**
+${refreshToken}
+
+**profile**
+${JSON.stringify(profile, null, 2)}
+`);
+  return done(null);
+});
+
+passport.use(googleStrategy);
+
 app.set('view engine', 'jade');
 
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.render('root/index');
 });
+
+app.get('/signin/google', passport.authenticate('google', {
+  scope: [
+    'https://www.googleapis.com/auth/userinfo.email',
+    'https://www.googleapis.com/auth/userinfo.profile'
+  ]
+}));
+app.get('/signin/google/callback', passport.authenticate('google'));
 
 app.listen(port, (err) => {
   if (err) {
